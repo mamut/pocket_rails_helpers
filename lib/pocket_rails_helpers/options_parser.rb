be819b42c682d177e605f2a676c3cce58af9ca2d
Krzysztof Urban <krzysztof.urban@netguru.pl>
2013-11-04 23:54:42 +0100

    satisfy specs

--- lib/pocket_rails_helpers/options_parser.rb
+++ lib/pocket_rails_helpers/options_parser.rb
@@ -1,4 +1,32 @@
 module PocketRailsHelpers
   class OptionsParser
+
+    attr_reader :options
+
+    def initialize(options)
+      @options = options
+    end
+
+    def call
+      {}.tap do |parsed|
+        parsed['save-url'] = link unless link.nil?
+        parsed['pocket-count'] = style unless style.nil?
+        parsed['pocket-align'] = align unless align.nil?
+      end
+    end
+
+    private
+
+    def link
+      options.fetch(:link) { nil }
+    end
+
+    def style
+      options.fetch(:style) { 'vertical' }
+    end
+
+    def align
+      options.fetch(:align) { 'left' if options[:style] == 'horizontal' }
+    end
   end
 end
--- spec/pocket_rails_helper/options_parser_spec.rb
+++ spec/pocket_rails_helper/options_parser_spec.rb
@@ -4,20 +4,52 @@ describe PocketRailsHelpers::OptionsParser do
 
   describe "link parsing" do
 
-    it "returns no save-url option if no link given"
-    it "returns save-url with link url if link given"
+    it "returns no save-url option if no link given" do
+      options = {}
+      parsed = described_class.new(options).call
+      expect(parsed).not_to have_key 'save-url'
+    end
+
+    it "returns save-url with link url if link given" do
+      options = { link: 'http://example.com' }
+      parsed = described_class.new(options).call
+      expect(parsed['save-url']).to eq 'http://example.com'
+    end
   end
 
   describe "style parsing" do
 
-    it "defaults to vertical if none given"
-    it "sets pocket-count to passed value if given"
+    it "defaults to vertical if none given" do
+      options = {}
+      parsed = described_class.new(options).call
+      expect(parsed['pocket-count']).to eq 'vertical'
+    end
+
+    it "sets pocket-count to passed value if given" do
+      options = { style: 'horizontal' }
+      parsed = described_class.new(options).call
+      expect(parsed['pocket-count']).to eq 'horizontal'
+    end
   end
 
   describe "alignment parsing" do
 
-    it "sets no alignment if none given"
-    it "defaults alignment to left if style is horizontal"
-    it "sets pocket-align to passed value if given"
+    it "sets no alignment if none given" do
+      options = {}
+      parsed = described_class.new(options).call
+      expect(parsed).not_to have_key 'pocket-align'
+    end
+
+    it "defaults alignment to left if style is horizontal" do
+      options = { style: 'horizontal' }
+      parsed = described_class.new(options).call
+      expect(parsed['pocket-align']).to eq 'left'
+    end
+
+    it "sets pocket-align to passed value if given" do
+      options = { align: 'right' }
+      parsed = described_class.new(options).call
+      expect(parsed['pocket-align']).to eq 'right'
+    end
   end
 end
