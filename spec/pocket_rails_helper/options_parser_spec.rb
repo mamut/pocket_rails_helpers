a0c4d1f9cf0f17f54e7b872c37ba163e7b75e684
Krzysztof Urban <krzysztof.urban@netguru.pl>
2013-11-04 23:43:16 +0100

    test cases

--- spec/pocket_rails_helper/options_parser_spec.rb
+++ spec/pocket_rails_helper/options_parser_spec.rb
@@ -2,5 +2,22 @@ require 'pocket_rails_helpers/options_parser'
 
 describe PocketRailsHelpers::OptionsParser do
 
-  it { expect(1).to eq 1 }
+  describe "link parsing" do
+
+    it "returns no save-url option if no link given"
+    it "returns save-url with link url if link given"
+  end
+
+  describe "style parsing" do
+
+    it "defaults to vertical if none given"
+    it "sets pocket-count to passed value if given"
+  end
+
+  describe "alignment parsing" do
+
+    it "sets no alignment if none given"
+    it "defaults alignment to left if style is horizontal"
+    it "sets pocket-align to passed value if given"
+  end
 end
