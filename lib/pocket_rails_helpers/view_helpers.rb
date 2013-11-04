5f6cbf35af9bee8c1734c9d8775b468bd1a69791
Krzysztof Urban <krzysztof.urban@netguru.pl>
2013-11-04 23:56:11 +0100

    use the parser for options

--- lib/pocket_rails_helpers/view_helpers.rb
+++ lib/pocket_rails_helpers/view_helpers.rb
@@ -1,13 +1,9 @@
+require 'pocket_rails_helpers/options_parser'
+
 module PocketRailsHelpers
   module ViewHelpers
     def pocket_button(options={})
-      link = options.fetch(:link) { nil }
-      style = options.fetch(:style) { 'vertical' }
-      align = options.fetch(:align) { 'left' if options[:style] == 'horizontal' }
-      pocket = {}
-      pocket['save-url'] = link if link.present?
-      pocket['pocket-count'] = style if style.present?
-      pocket['pocket-align'] = align if align.present?
+      pocket = OptionsParser.new(options).call
       link_to 'Pocket', 'https://getpocket.com/save', class: 'pocket-btn', data: pocket
     end
   end
