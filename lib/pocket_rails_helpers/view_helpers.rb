0e5ff1df195afc452e70f9c17b88192de150cf6e
Krzysztof Urban <krzysztof.urban@netguru.pl>
2013-11-04 23:15:49 +0100

    generate proper data attributes

--- lib/pocket_rails_helpers/view_helpers.rb
+++ lib/pocket_rails_helpers/view_helpers.rb
@@ -1,7 +1,14 @@
 module PocketRailsHelpers
   module ViewHelpers
-    def pocket_button(link, options={})
-      content_tag :span, link + options.to_param
+    def pocket_button(options={})
+      link = options.fetch(:link) { nil }
+      style = options.fetch(:style) { 'vertical' }
+      align = options.fetch(:align) { 'left' if options[:style] == 'horizontal' }
+      pocket = {}
+      pocket['save-url'] = link if link.present?
+      pocket['pocket-count'] = style if style.present?
+      pocket['pocket-align'] = align if align.present?
+      link_to 'Pocket', 'https://getpocket.com/save', class: 'pocket-btn', data: pocket
     end
   end
 end
